b582608cc2529433dc0502371b92429db5da0851
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2023-09-06 21:14:35 +0900

    Updated db config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,27 @@
-module.exports = ({ env }) => ({
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST','139.59.102.235'),
+//       port: env.int('DATABASE_PORT',3306),
+//       database: env('DATABASE_NAME','strapi'),
+//       user: env('DATABASE_USERNAME','myadmin2'),
+//       password: env('DATABASE_PASSWORD','MySQL@dmin!12345'),
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//       },
+//     },
+//     debug: false,
+//   },
+// });
+
+odule.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST','139.59.102.235'),
-      port: env.int('DATABASE_PORT',3306),
-      database: env('DATABASE_NAME','strapi'),
-      user: env('DATABASE_USERNAME','myadmin2'),
-      password: env('DATABASE_PASSWORD','MySQL@dmin!12345'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
     },
+    useNullAsDefault: true,
     debug: false,
   },
 });
